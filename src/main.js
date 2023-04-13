375d19db42719827012a1d2396b3843d74be04f2
Robyn Drake <robin.drake.041@gmail.com>
2023-04-13 12:11:16 -0500

    further work

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import {
+	getSolidDataset,
+	getThing,
+	setThing,
+	getStringNoLocale,
+	setStringNoLocale,
+	saveSolidDatasetAt
+} from "@inrupt/solid-client";
+import { Session } from "@inrupt/solid-client-authn-browser";
+import { VCARD } from "@inrupt/vocab-common-rdf";
+
+
